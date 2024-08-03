5f8f8c96f954efb7650b28550aae39a1349d41f8
Anthony <ndegwaanthony300@gmail.com>
2024-08-03 18:57:39 +0300

    2. Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
-"""filtered_logger.py"""
+
 import logging
 import re
+from typing import List
+
+# Define PII_FIELDS constant
+PII_FIELDS = ("name", "email", "ssn", "password", "phone")
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """Redact sensitive information in a log message."""
     pattern = "({})=[^{}]+".format('|'.join(fields), separator)
     return re.sub(pattern, lambda m: "{}={}".format(m.group(1), redaction),
                   message)
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
+    """Redacting Formatter class."""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
@@ -29,11 +35,23 @@ class RedactingFormatter(logging.Formatter):
         return super(RedactingFormatter, self).format(record)
 
 
+def get_logger() -> logging.Logger:
+    """Creates and configures a logger."""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
 # Example Usage
 if __name__ == "__main__":
-    message = ("name=Bob;email=bob@dylan.com;ssn=000-123-0000;password="
-               "bobby2019;")
-    log_record = logging.LogRecord("my_logger", logging.INFO, None, None,
-                                   message, None, None)
-    formatter = RedactingFormatter(fields=("email", "ssn", "password"))
-    print(formatter.format(log_record))
+    logger = get_logger()
+    logger.info("name=John Doe;email=johndoe@example.com;ssn=123-45-6789;"
+                "password=secret;phone=555-555-5555;")
