f4a75b16d8d36eeffabca2a2ac8f1805661d713e
Anthony <ndegwaanthony300@gmail.com>
2024-08-07 14:59:37 +0300

    5. Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 import logging
+from api.v1.auth.auth import Auth
 
 
 # logging.basicConfig(level=logging.DEBUG,filename='test.log')
@@ -16,7 +17,12 @@ import logging
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
+
+AUTH_TYPE = os.getenv('AUTH_TYPE')
+if AUTH_TYPE == 'auth':
+    auth = Auth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -35,6 +41,21 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """restricted to some resources (403)"""
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """ Filter each request before handling it
+    """
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,11 +8,24 @@ class Auth:
     """Auth class init"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """authentication is required"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path_split in excluded_paths:
+            path_split = path_split.strip('*')
+            if path_split.startswith(path_split):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """header authorization"""
-        return None
+        if request is None:
+            return None
+        if not request.authorization
+            return None
+        item = request.authorization()
+        return item
 
     def current_user(self, request=None) -> TypeVar('User'):
         """get the current_user"""
