f18b550c91ed5d810299b2afc38eaff07bc91af3
Anthony <ndegwaanthony300@gmail.com>
2024-08-07 15:01:45 +0300

    5. Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python3
-"""auth module"""
+'''auth module
+'''
+from typing import List, TypeVar
 from flask import request
-from typing import TypeVar
 
 
 class Auth:
-    """Auth class init"""
+    '''auth class
+    '''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """authentication is required"""
+        '''confirms if path is in exclluded paths
+        '''
         if path is None:
             return True
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
-        if path_split in excluded_paths:
-            path_split = path_split.strip('*')
-            if path_split.startswith(path_split):
+        for item in excluded_paths:
+            # if item.startswith(path):
+            item = item.strip('*')
+            if path.startswith(item):
                 return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """header authorization"""
+        '''gets the data from authorization header bit
+        '''
         if request is None:
             return None
-        if not request.authorization
-            return None
-        item = request.authorization()
-        return item
+        value = request.authorization
+        return value
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """get the current_user"""
+        '''gets current user
+        '''
         return None
