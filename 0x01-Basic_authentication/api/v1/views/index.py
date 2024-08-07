4ce2b7d0aab69340c706e630cf05dbe223d59d19
Anthony <ndegwaanthony300@gmail.com>
2024-08-07 13:24:36 +0300

    strict_slashes of the route

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'])
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized():
     """401 error"""
 
