a8ce7840d4b6c1f357d9ba7724bd8943364abfb5
Anthony <ndegwaanthony300@gmail.com>
2024-08-07 13:08:05 +0300

    initial commit

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+import logging
 
 
 app = Flask(__name__)
@@ -22,10 +23,9 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error):
-    response = jsonify({"error": "Unauthorized"})
-    response.status_code = 401
-    return response
+def unauthorized(error) -> str:
+    """unauthorized handler"""
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
