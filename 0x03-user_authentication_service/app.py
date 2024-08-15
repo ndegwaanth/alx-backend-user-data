2b3bdfc36b4ffe827961a165da503bc3ab397c91
Anthony <ndegwaanthony300@gmail.com>
2024-08-15 17:00:38 +0300

    implement the end-point to register a user. Define a users function that implements the POST /users route.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python3
 """6. Basic Flask app"""
-from flask import Flask
+from flask import Flask, request, jsonify
 import flask
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'])
 def index():
     """returning json payload"""
     return flask.jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'])
+def user():
+    """
+    implement the end-point to register a user. Define a users function
+    that implements the POST /users route.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == '__main__':
