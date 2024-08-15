ff1e75fc1d1ef12c6d641ccc09332a46fcbd77c1
Anthony <ndegwaanthony300@gmail.com>
2024-08-15 13:03:40 +0300

    remove the def __init__ that prevent accessing partial of the data in the User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -17,10 +17,3 @@ class User(Base):
     hashed_password = Column("hashed_password", String(250), nullable=False)
     session_id = Column("session_id", String(250), nullable=True)
     reset_token = Column("reset_token", String(250), nullable=True)
-
-    def __init__(self, id, email, hashed_password, session_id, reset_token):
-        self.id = id
-        self.email = email
-        self.hashed_password = hashed_password
-        self.session_id = session_id
-        self.reset_token = reset_token
